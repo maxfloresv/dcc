5c1b43a74094f9e08e6dfda3df39eaef04a9c076
Máximo Flores Valenzuela <maximo.flores@ug.uchile.cl>
2023-06-25 18:59:21 -0400

    testcase #3

---
+++ CC3101/tests/3.py
@@ -0,0 +1,26 @@
+while x > 3:
+	x = 4
+	if y > 2:
+		a = 10
+	if x > y:
+		z = 2
+		if x > 8:
+			o = 1
+		if x > 6:
+			o = 0
+		else:
+			a -= 1298
+			if x < 3:
+				t = 666
+			else:
+				t -= 1
+	else:
+		x = 6
+		a = 1
+		if a < 2:
+			a = 1
+		if x > 2:
+			a -= 2
+		if x > -1:
+			a -= 2
+	a = 2
